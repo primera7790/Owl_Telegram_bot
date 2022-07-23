59b3802f99dcb28e6933e89c303e373057e10305
primera | <def.primera@gmail.com>
2022-07-23 15:22:25 +0500

    First commit

---
+++ owl_bot.py
@@ -0,0 +1,86 @@
+import telebot
+from telebot.types import Message
+import os
+from dotenv import load_dotenv
+import else_type
+import random
+
+load_dotenv()
+
+TOKEN = os.environ.get('O_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.from_user.id, 'Какой вопрос привёл тебя ко мне?')
+
+
+@bot.message_handler(content_types=[
+    'text',
+    'audio',
+    'document',
+    'photo',
+    'sticker',
+    'video',
+    'video_note',
+    'voice',
+    'location',
+    'contact',
+    'new_chat_members',
+    'left_chat_member',
+    'new_chat_title',
+    'new_chat_photo',
+    'delete_chat_photo',
+    'group_chat_created',
+    'supergroup_chat_created',
+    'channel_chat_created',
+    'migrate_to_chat_id',
+    'migrate_from_chat_id',
+    'pinned_message'
+])
+def echo_all(message: Message):
+    if message.content_type == 'text':
+        if len(message.text) > 3:
+
+            questions_key_words = {
+                'согласен': 'Согласен!',
+                'правильно': 'Правильно!',
+                'поддерживаешь': 'Поддерживаю!'
+            }
+            answers = [
+                'Угу',
+                'Так и есть!',
+                'Не поспоришь',
+                'Конечно!',
+                'А как иначе',
+                'Агась',
+                'А кто-то думает иначе??',
+                'ОО, да!',
+                'Угу',
+                'Безусловно',
+                'Вот тут я, пожалуй, отмолчусь..',
+                'Главное, что ты в это веришь!',
+                'Скажешь тоже..',
+                'Угу'
+            ]
+            cycles = 0
+            for i in questions_key_words:
+                if message.text.lower().find(i) > -1:
+                    bot.send_message(message.from_user.id, questions_key_words[i])
+                else:
+                    cycles += 1
+                if cycles == len(questions_key_words):
+                    bot.send_message(message.from_user.id, random.choice(answers))
+
+            with open('num_incorrect_request.txt', 'w') as f:
+                f.write(str(0))
+                f.close()
+
+        else:
+            else_type.else_type(message)
+    else:
+        else_type.else_type(message)
+
+
+bot.infinity_polling()
